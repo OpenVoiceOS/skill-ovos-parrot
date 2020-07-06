aaa3dc76fe6f8460deaaa80f6d137a44202903be
jarbasal <jarbasai@mailfence.com>
2020-07-06 01:59:17 +0100

    modernize

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 from mycroft.skills.core import MycroftSkill, intent_file_handler
-
-__author__ = 'jarbas'
+from os.path import join, dirname
+from os import listdir
+from mycroft.skills.core import resting_screen_handler
+import random
 
 
 class ParrotSkill(MycroftSkill):
-    parroting = False
+    def __init__(self):
+        super(ParrotSkill, self).__init__("ParrotSkill")
+        self.parroting = False
+        self.heard_utts = []
 
     @intent_file_handler("start_parrot.intent")
     def handle_start_parrot_intent(self, message):
@@ -26,12 +31,28 @@ class ParrotSkill(MycroftSkill):
             return True
         return False
 
+    def update_picture(self, utterance=None):
+        if len(self.heard_utts):
+            utterance = utterance or random.choice(self.heard_utts)
+        path = join(dirname(__file__), "ui", "parrots")
+        pic = join(path, random.choice(listdir(path)))
+        self.gui.show_image(pic, caption=utterance,
+                            fill='PreserveAspectFit')
+
+    # idle screen
+    @resting_screen_handler("Parrots")
+    def idle(self):
+        self.update_picture()
+
     def converse(self, utterances, lang="en-us"):
+        self.heard_utts += utterances
         if self.parroting:
             # check if stop intent will trigger
-            if self.voc_match(utterances[0], "StopKeyword") and self.voc_match(utterances[0], "ParrotKeyword"):
+            if self.voc_match(utterances[0], "StopKeyword") and \
+                    self.voc_match(utterances[0], "ParrotKeyword"):
                 return False
             # if not parrot utterance back
+            self.update_picture(utterances[0])
             self.speak(utterances[0], expect_response=True)
             return True
         else:
