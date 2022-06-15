1f89b6c25a29da4d13ddbaeec2e99c207851e012
jarbasai <jarbasai@mailfence.com>
2022-06-15 17:20:52 +0100

    session refactor

--- __init__.py
+++ __init__.py
@@ -1,14 +1,29 @@
 from monotonic import monotonic
 from mycroft.skills.core import MycroftSkill, intent_file_handler
+from mycroft_bus_client.message import dig_for_message
+
+
+class ParrotSession:
+    def __init__(self, session_id):
+        self.session_id = session_id
+        self.parroting = False
+        self.heard_utts = []
+        self.last_tts = []
+        self.last_stt_time = []
+
+    @property
+    def previous_stt(self):
+        # last is current utt
+        if len(self.heard_utts) < 2:
+            return None
+        else:
+            return self.heard_utts[-2]
 
 
 class ParrotSkill(MycroftSkill):
     def __init__(self):
+        self.sessions = {"default": ParrotSession("default")}
         super(ParrotSkill, self).__init__("ParrotSkill")
-        self.parroting = False
-        self.heard_utts = {"_all": []}
-        self.last_tts = {"_all": []}
-        self.last_stt_time = {"_all": 0}
 
     def initialize(self):
         # events used in intents rom
@@ -16,43 +31,38 @@ class ParrotSkill(MycroftSkill):
         self.add_event('recognizer_loop:utterance', self.on_utterance)
         self.add_event('speak', self.on_speak)
 
+    def get_session(self, message):
+        # use context session id, or fallback to utterance source (client id)
+        # utterance source may be a list
+        # see https://github.com/OpenVoiceOS/ovos-core/pull/160
+        s = message.context.get("session_id") or \
+            message.context.get("source")
+
+        # create session objects
+        if isinstance(s, list):
+            for sess in s:
+                if sess not in self.sessions:
+                    self.sessions[sess] = ParrotSession(sess)
+            # TODO what now? 1st? last?
+            # this is a fallback to track different clients
+            # cast to default session/treat as broadcast instead?
+            return self.sessions["default"]
+
+        elif s not in self.sessions:
+            self.sessions[s] = ParrotSession(s)
+
+        return self.sessions[s]
+
     def on_utterance(self, message):
         ts = monotonic()
-        sources = message.context.get("source", "broadcast")
-        if isinstance(sources, str):
-            sources = [sources]
-
-        for source in sources:
-            if source not in self.heard_utts:
-                self.heard_utts[source] = []
-            self.heard_utts[source] += message.data['utterances']
-            self.heard_utts["_all"] += message.data['utterances']
-            self.last_stt_time[source] = self.last_stt_time["_all"] = ts
-            #self.log.info(f"{source} utterance: {message.data['utterances']}")
-            if source == "broadcast":
-                for s in self.heard_utts:
-                    if s == source:
-                        continue
-                    self.heard_utts[s] += message.data['utterances']
-                for s in self.last_stt_time:
-                    if s == source:
-                        continue
-                    self.last_stt_time[s] = ts
+        sess = self.get_session(message)
+        sess.last_stt_time = ts
+        utt = message.data['utterances'][0]
+        sess.heard_utts.append(utt)
 
     def on_speak(self, message):
-        sources = message.context.get("destination", "broadcast")
-        if isinstance(sources, str):
-            sources = [sources]
-        for source in sources:
-            if source not in self.last_tts:
-                self.last_tts[source] = ""
-            self.last_tts[source] = self.last_tts["_all"] = message.data['utterance']
-            #self.log.info(f"{source} speak: {message.data['utterance']}")
-            if source == "broadcast":
-                for s in self.last_tts:
-                    if s == source:
-                        continue
-                    self.last_tts[s] = message.data['utterance']
+        sess = self.get_session(message)
+        sess.last_tts = message.data['utterance']
 
     # Intents
     @intent_file_handler("speak.intent")
@@ -64,36 +74,16 @@ class ParrotSkill(MycroftSkill):
     @intent_file_handler('repeat.tts.intent')
     def handle_repeat_tts(self, message):
         # replaces https://github.com/MatthewScholefield/skill-repeat-recent
-        sources = message.context.get("destination", ["broadcast"])
-        if isinstance(sources, str):
-            sources = [sources]
-        utts = []
-        for source in sources:
-            utt = self.last_tts.get(source)
-            if utt:
-                utts.append(utt)
-        if len(utts) < 1:
-            last_tts = self.translate('nothing')
-        else:
-            last_tts = utts[-1]  # last is current utt
-        self.speak_dialog('repeat.tts',
-                          {"tts": last_tts})
+        sess = self.get_session(message)
+        utt = sess.last_tts or self.translate('nothing')
+        self.speak_dialog('repeat.tts', {"tts": utt})
 
     @intent_file_handler('repeat.stt.intent')
     def handle_repeat_stt(self, message):
         # replaces https://github.com/MatthewScholefield/skill-repeat-recent
-        sources = message.context.get("destination", ["broadcast"])
-        if isinstance(sources, str):
-            sources = [sources]
-        utts = []
-        for source in sources:
-            utts += self.heard_utts.get(source, [])
-        ts = max([self.last_stt_time.get(source) or 0 for source in sources])
-        if len(utts) < 2:
-            last_stt = self.translate('nothing')
-        else:
-            last_stt = utts[-2]  # last is current utt
-        if monotonic() - ts > 120:
+        sess = self.get_session(message)
+        last_stt = sess.previous_stt or self.translate("nothing")
+        if monotonic() - sess.last_stt_time > 120:
             self.speak_dialog('repeat.stt.old', {"stt": last_stt})
         else:
             self.speak_dialog('repeat.stt', {"stt": last_stt})
@@ -101,31 +91,19 @@ class ParrotSkill(MycroftSkill):
     @intent_file_handler('did.you.hear.me.intent')
     def handle_did_you_hear_me(self, message):
         # replaces https://github.com/MatthewScholefield/skill-repeat-recent
-        sources = message.context.get("destination", ["broadcast"])
-        if isinstance(sources, str):
-            sources = [sources]
-        ts = max([self.last_stt_time.get(source, 0) for source in sources])
-
-        if monotonic() - ts > 60 or len(self.heard_utts) == 0:
+        sess = self.get_session(message)
+        if monotonic() - sess.last_stt_time > 60 or not sess.previous_stt:
             self.speak_dialog('did.not.hear')
             self.speak_dialog('please.repeat', expect_response=True)
         else:
-            utts = []
-            for source in sources:
-                utts += self.heard_utts.get(source, [])
-
-            if len(utts) < 2:
-                last_stt = self.translate('nothing')
-            else:
-                last_stt = utts[-2]  # last is current utt
-
             self.speak_dialog('did.hear')
-            self.speak_dialog('repeat.stt', {"stt": last_stt})
+            self.speak_dialog('repeat.stt', {"stt": sess.previous_stt})
 
     # continuous conversation
     @intent_file_handler("start_parrot.intent")
     def handle_start_parrot_intent(self, message):
-        self.parroting = True
+        sess = self.get_session(message)
+        sess.parroting = True
         self.speak_dialog("parrot_start", expect_response=True)
         self.gui["running"] = False
         self.gui.show_page("parrot.qml", override_idle=True)
@@ -138,7 +116,10 @@ class ParrotSkill(MycroftSkill):
             self.speak_dialog("not_parroting")
 
     def converse(self, utterances, lang="en-us"):
-        if self.parroting:
+        # TODO message from kwargs
+        message = dig_for_message()
+        sess = self.get_session(message)
+        if sess.parroting:
             # check if stop intent will trigger
             if self.voc_match(utterances[0], "StopKeyword") and \
                     self.voc_match(utterances[0], "ParrotKeyword"):
@@ -150,8 +131,10 @@ class ParrotSkill(MycroftSkill):
             return False
 
     def stop(self):
-        if self.parroting:
-            self.parroting = False
+        message = dig_for_message()
+        sess = self.get_session(message)
+        if sess.parroting:
+            sess.parroting = False
             self.speak_dialog("parrot_stop")
             self.gui["running"] = False
             self.gui.release()
