3c8f379c4e310d6a88339eb089262f26f7be5499
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-12-31 01:07:12 +0000

    refactor/session_awareness (#6)
    * refactor/session_awareness
    
    * refactor/session_awareness
    
    * remove expect_responses
    
    * deactivate parrot mode when skill is no longer active
    
    * simplify
    
    * support for https://github.com/OpenVoiceOS/OVOS-workshop/pull/169

--- __init__.py
+++ __init__.py
@@ -151,13 +151,15 @@ class ParrotSkill(OVOSSkill):
         self.stop_session(sess)
 
     def stop_session(self, session: Session):
-        if session.session_id in self.parrot_sessions and\
+        if session.session_id in self.parrot_sessions and \
                 self.parrot_sessions[session.session_id]["parrot"]:
             self.parrot_sessions[session.session_id]["parrot"] = False
             self.speak_dialog("parrot_stop")
             if session.session_id == "default":
                 self.gui["running"] = False
                 self.gui.release()
+            return True
+        return False
 
     def stop(self):
         sess = SessionManager.get()
